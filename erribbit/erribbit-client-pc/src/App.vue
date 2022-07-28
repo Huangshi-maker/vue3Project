8d0b690671227ca88fe0671ed779c6603b1d89e5
hs <1746788127@qq.com>
2022-07-28 09:46:47 +0800

    rabbit

---
+++ erribbit/erribbit-client-pc/src/App.vue
@@ -0,0 +1,22 @@
+<template lang="">
+    <div>
+        <!-- 使用A模块的state数据 -->
+    <p>{{$store.state.moduleA.username}}</p>
+        <!-- 使用A模块的getters数据 -->
+        <p>{{$store.getters.newName}}</p>
+
+        <!-- 使用B模块的state数据 -->
+    <p>{{$store.state.moduleB.username}}</p>
+            <!-- 使用B模块的getters数据 -->
+        <p>{{$store.getters['moduleB/newName']}}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'App',
+
+}
+</script>
+<style lang="">
+
+</style>
