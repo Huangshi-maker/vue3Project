fdebf1244c95af283292536f5e1d76df7308acfc
hs <1746788127@qq.com>
2022-07-28 09:55:12 +0800

    实战二

---
+++ 小兔鲜/demo/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-view />
+</template>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+.flex-float {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.flex {
+  display: flex;
+  align-items: center;
+}
+</style>
---
+++ 小兔鲜/erribbit-client-pc/src/根模块的vuex使用方法.vue
@@ -0,0 +1,38 @@
+<template lang="">
+  <div>
+    App
+    <!-- 使用根模块的state数据 -->
+    <p>{{$store.state.username}}</p>
+    <!-- 使用根模块gettes的数据 -->
+    <p>{{$store.getters.newname}}</p>
+    <button type="primary" size="default" @click="mutationsFn">clickme</button>
+
+  </div>
+
+</template>
+<script>
+import { useStore } from 'vuex'
+export default {
+  name: 'App',
+  setup () {
+    // 使用vuex仓库
+    const store = useStore()
+    // 使用根模块state数据
+    console.log(store.state.username)
+    // 使用根模块的getters的数据
+    console.log(store.getters.newname)
+    // 提交根模块mutations函数
+    const mutationsFn = () => {
+      // store.commit('updateName')
+
+      // 调用根模块actions函数
+      store.dispatch('updateName')
+    }
+
+    return {
+      mutationsFn
+    }
+  }
+
+}
+</script>
